7b5d401155a561c0b2371a8f1db2516e9edbb56a
Daniel Kwon <34282186+danielmsk@users.noreply.github.com>
2020-01-23 18:02:30 -0500

    Update s01_make_inputVCF_and_runscript.sh

--- scripts/precal_vep/s01_make_inputVCF_and_runscript.sh
+++ scripts/precal_vep/s01_make_inputVCF_and_runscript.sh
@@ -5,7 +5,7 @@
 #     -vep /home/mk446/bio/mutanno/ANNOT3TOOLS/BIN/ensembl-vep-release-98/vep \
 #     -cache_version 98
 
-python mutanno.py precal -run_vep \
+mutanno precal -run_vep \
     -out /home/mk446/mutanno/PRECALVEP \
     -fasta /n/data1/hms/dbmi/park/SOFTWARE/REFERENCE/GRCh38d1/GRCh38_full_analysis_set_plus_decoy_hla.fa \
     -vepcache /home/mk446/bio/mutanno/ANNOT3TOOLS/BIN/nonindexed_vep_cache/homo_sapiens_vep \
