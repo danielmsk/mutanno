4d732c7361fe07e46f1936e25755ee969446fe9a
danielmsk <daniel.minseok.kwon@gmail.com>
2020-03-05 16:12:34 -0500

    add default value

--- scripts/make_datasource/tmp_add_na_microannot.sh_03.sh
+++
@@ -1,1 +0,0 @@
-python tmp_add_na_microannot.py /home/mk446/mutanno/DATASOURCE/MICROANNOT/v0.2_200211/microannot_datasource.11.v0.2_200211.tsi.gz
--- scripts/make_datasource/tmp_add_na_microannot.sh_23.sh
+++
@@ -1,1 +0,0 @@
-python tmp_add_na_microannot.py /home/mk446/mutanno/DATASOURCE/MICROANNOT/v0.2_200211/microannot_datasource.M.v0.2_200211.tsi.gz
