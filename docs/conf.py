e2b50221d51b5217b48bf3ad9baf0c9d9be2bfad
Daniel Kwon <34282186+danielmsk@users.noreply.github.com>
2020-07-31 13:59:13 -0400

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -30,9 +30,15 @@ release = '0.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx_rtd_theme",
+extensions = ['sphinx_rtd_theme', 
+	"sphinx.ext.intersphinx", 
+	'sphinx.ext.autodoc', 
+	'sphinx.ext.viewcode', 
+	'sphinx.ext.todo'
 ]
+
+# The master toctree document.
+master_doc = 'index'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -55,3 +61,19 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ['_static']
+
+html_theme_options = {
+    'canonical_url': '',
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'black',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
